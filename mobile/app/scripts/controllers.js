94c2bd60cad5f65d34eaffd868326ec6b14e935b
Xavier Haniquaut <xavier.haniquaut@gmail.com>
2015-11-14 00:31:09 +0100

    Some tests with the front

--- mobile/app/scripts/controllers.js
+++ mobile/app/scripts/controllers.js
@@ -1,58 +1,63 @@
-'use strict';
+(function(){
+  'use strict';
 
-angular.module('starter.controllers', [])
+  angular
+    .module('starter.controllers', [])
+    .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+      // With the new view caching in Ionic, Controllers are only called
+      // when they are recreated or on app start, instead of every page change.
+      // To listen for when this page is active (for example, to refresh data),
+      // listen for the $ionicView.enter event:
+      //$scope.$on('$ionicView.enter', function(e) {
+      //});
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+      // Form data for the login modal
+      $scope.loginData = {};
 
-  // Form data for the login modal
-  $scope.loginData = {};
+      // Create the login modal that we will use later
+      $ionicModal.fromTemplateUrl('templates/login.html', {
+        scope: $scope
+      }).then(function(modal) {
+        $scope.modal = modal;
+      });
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+      // Triggered in the login modal to close it
+      $scope.closeLogin = function() {
+        $scope.modal.hide();
+      };
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+      // Open the login modal
+      $scope.login = function() {
+        $scope.modal.show();
+      };
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+      // Perform the login action when the user submits the login form
+      $scope.doLogin = function() {
+        console.log('Doing login', $scope.loginData);
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+        // Simulate a login delay. Remove this and replace with your login
+        // code if using a login system
+        $timeout(function() {
+          $scope.closeLogin();
+        }, 1000);
+      };
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
+      $scope.trips = [
+        { id: 1, label: 'Andalousie' },
+        { id: 2, label: 'Autriche' },
+        { id: 3, label: 'Bruxelles' },
+      ];
+    })
+    .controller('PlaylistsCtrl', function($scope) {
+      $scope.playlists = [
+        { title: 'trajet1', id: 1 },
+        { title: 'trajet2', id: 2 },
+        { title: 'trajet3', id: 3 },
+      ];
+    })
+    .controller('PlaylistCtrl', function($scope, $stateParams) {
+      $scope.idPlaylist = $stateParams.playlistId;
+    });
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
-
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+}());
