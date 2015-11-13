e30fcdfef08de3d57a67b88beadfe2e31e5b96c6
Sébastien Elet <sebastien@elet.fr>
2015-11-14 00:47:29 +0100

    feat(backend): add twitter logger

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,37 @@ var initRoutes = require('./app/routes');
 var Promise = require('bluebird');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
+var winston = require('winston');
+var Twitter = require('twitter');
+var util = require('util');
 
+// Twitter logger transport
+var TwittLog = exports.TwittLog = function TwittLog(options) {
+  this.client = new Twitter(options || {});
+};
+
+util.inherits(TwittLog, winston.Transport);
+TwittLog.prototype.log = function log(level, msg, meta, callback) {
+  var self = this; // eslint-disable-line
+
+  msg = msg.substring(0, 128) + '… #jdmc15';
+  this.client.post('statuses/update', { status: msg }, function twitterCb(error) {
+    if (error) {
+      console.log(error); // eslint-disable-line no-console
+      self.emit('error', error);
+    }
+  });
+
+  this.emit('logged');
+
+  callback(null, true);
+};
+winston.add(TwittLog, {
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_TOKEN_SECRET,
+});
 
 // Preparing services
 Promise.all([
@@ -20,6 +50,7 @@ Promise.all([
   context.app = express();
   context.createObjectId = ObjectId;
   context.castToObjectId = ObjectId;
+  context.logger = winston;
   context.checkAuth = function isLoggedIn(req, res, next) {
     console.log('isLoggedIn', req.isAuthenticated(), req.user);
     if(req.user || req.isAuthenticated()) {
@@ -45,9 +76,8 @@ Promise.all([
         return reject(err);
       }
 
-      console.log('Patrick is here -> http://%s:%s', context.host, context.port);
+      context.logger.info('Patrick is here \\o/ -> http://%s:%s', context.host, context.port);
       resolve();
     });
   });
-
 });
