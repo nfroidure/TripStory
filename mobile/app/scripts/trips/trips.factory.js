1a7e7fe1c0b301bdaa656d5752f30bf2320cdcd8
Xavier Haniquaut <xavier.haniquaut@gmail.com>
2015-11-15 16:58:20 +0100

    Add members avatar to trip desc block

--- mobile/app/scripts/trips/trips.factory.js
+++ mobile/app/scripts/trips/trips.factory.js
@@ -8,10 +8,36 @@
   tripsFactory.$inject = [];
   /* @ngInject */
   function tripsFactory() {
+      // will be repaced by the backend
       var data = [
-        { id: 1, label: 'Andalousie', description: 'La description du trip en Andalousie', coverPicture: 'https://placekitten.com/g/500/299' },
-        { id: 2, label: 'Autriche',   description: 'La description du trip en Autriche'  , coverPicture: 'https://placekitten.com/g/500/300' },
-        { id: 3, label: 'Bruxelles',  description: 'La description du trip à Bruxelles'  , coverPicture: 'https://placekitten.com/g/500/301' },
+        {
+          id: 1,
+          label: 'Andalousie',
+          description: 'La description du trip en Andalousie',
+          coverPicture: 'https://placekitten.com/g/500/299',
+          members: [
+            { id: 1, name: 'Nicolas', avatarPicture: 'https://placekitten.com/g/40/41' },
+            { id: 2, name: 'Remy', avatarPicture: 'https://placekitten.com/g/40/39' },
+            { id: 3, name: 'Xavier', avatarPicture: 'https://placekitten.com/g/39/40' },
+          ],
+        },
+        {
+          id: 2,
+          label: 'Autriche',
+          description: 'La description du trip en Autriche',
+          coverPicture: 'https://placekitten.com/g/500/300',
+          members: [{ id: 4, name: 'Marc', avatarPicture: 'https://placekitten.com/g/40/40' }],
+        },
+        {
+          id: 3,
+          label: 'Bruxelles',
+          description: 'La description du trip à Bruxelles',
+          coverPicture: 'https://placekitten.com/g/500/301',
+          members: [
+            { id: 5, name: 'Sebastien', avatarPicture: 'https://placekitten.com/g/40/42' },
+            { id: 2, name: 'Remy', avatarPicture: 'https://placekitten.com/g/40/39' },
+          ],
+        },
       ];
       var service = {
         get: get,
