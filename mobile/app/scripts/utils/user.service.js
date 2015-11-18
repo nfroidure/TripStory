713508e3bf80f508adc2d8cefbeec25d2aa6ed43
Xavier Haniquaut <xavier.haniquaut@gmail.com>
2015-11-18 07:52:17 +0100

    Some more fixes service related

---
+++ mobile/app/scripts/utils/user.service.js
@@ -0,0 +1,27 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app.utils')
+    .factory('userService', userService);
+
+  userService.$inject = ['$http', 'ENV', 'AuthService'];
+  /* @ngInject */
+  function userService($http, ENV, AuthService) {
+    var apiEndPoint = ENV.apiEndpoint;
+    var userId = '';
+    var service = {
+      getUser: getUser,
+    }
+    return service;
+
+    ////////////////
+    function getUser(userId) {
+      return AuthService.userIdPromise.then(function(userId){
+        var url = ENV.apiEndpoint + 'api/v0/users/' + userId;
+        return $http.get(url);
+      });
+    }
+  }
+
+}());
