2437bb7d097c03e9945421458ff6ed68768c40df
Nicolas Froidure <nicolas.froidure@insertafter.com>
2016-03-27 19:47:28 +0200

    Better UI for profile and cars

--- mobile/app/scripts/cars/cars.controller.js
+++ mobile/app/scripts/cars/cars.controller.js
@@ -5,16 +5,26 @@
     .module('app.cars')
     .controller('CarsCtrl', CarsCtrl);
 
-  CarsCtrl.$inject = ['$scope', '$state', '$stateParams', 'carsFactory'];
+  CarsCtrl.$inject = [
+    '$scope', '$state', '$stateParams', 'carsFactory', 'AuthService', 'ENV',
+  ];
   /* @ngInject */
-  function CarsCtrl($scope, $state, $stateParams, carsFactory) {
+  function CarsCtrl(
+    $scope, $state, $stateParams, carsFactory, AuthService, ENV
+  ) {
+    $scope.apiEndpoint = ENV.apiEndpoint;
     $scope.cars = [];
+    $scope.year = (new Date()).getFullYear();
     $scope.state = 'loading';
 
     activate();
 
     function activate() {
       $scope.state = 'loading';
+      AuthService.getProfile()
+      .then(function(profile) {
+        $scope.profile = profile;
+      });
       carsFactory.list()
         .then(function(cars){
           $scope.cars = cars.data;
