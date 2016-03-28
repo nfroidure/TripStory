cbabe1fd63d7df4f6e1bba904e7019685e568fac
Nicolas Froidure <nicolas.froidure@insertafter.com>
2016-03-28 15:45:21 +0200

    Allowing to remove a car from the interface

--- mobile/app/scripts/cars/cars.controller.js
+++ mobile/app/scripts/cars/cars.controller.js
@@ -16,6 +16,7 @@
     $scope.cars = [];
     $scope.year = (new Date()).getFullYear();
     $scope.state = 'loading';
+    $scope.remove = remove;
     $scope.refresh = activate;
 
     activate();
@@ -30,6 +31,17 @@
         .then(function(cars){
           $scope.cars = cars.data;
           $scope.state = 'loaded';
+        })
+        .catch(function(err) {
+          $scope.state = 'errored';
+        });
+    }
+
+    function remove(id) {
+      $scope.state = 'loading';
+      carsFactory.remove(id)
+        .then(function(){
+          $scope.refresh();
         })
         .catch(function(err) {
           $scope.state = 'errored';
--- mobile/app/scripts/cars/cars.factory.js
+++ mobile/app/scripts/cars/cars.factory.js
@@ -10,6 +10,8 @@
   function carsFactory($http, createObjectId , $q, ENV, AuthService) {
       var service = {
         list: list,
+        get: get,
+        remove: remove,
       };
 
       return service;
@@ -18,7 +20,38 @@
       function list() {
         return AuthService.getProfile().then(function(profile){
           var url = ENV.apiEndpoint + '/api/v0/users/' + profile._id + '/cars';
-          return $http.get(url);
+          return $http.get(url).then(function(response) {
+            if(200 !== response.status) {
+              throw response;
+            }
+            return response;
+          });
+        });
+      }
+
+      function get(id) {
+        return AuthService.getProfile().then(function(profile){
+          var url = ENV.apiEndpoint + '/api/v0/users/' + profile._id + '/cars/' + id;
+          return $http.get(url).then(function(response) {
+            if(200 !== response.status) {
+              throw response;
+            }
+            return response;
+          });
+        });
+      }
+
+      function remove(id) {
+        return AuthService.getProfile().then(function(profile){
+          var url = ENV.apiEndpoint + '/api/v0/users/' + profile._id + '/cars/' + id;
+          return $http.delete(url).then(function(response) {
+            throw response;
+          }).catch(function(response) {
+            if(410 !== response.status) {
+              throw response;
+            }
+            return response;
+          });
         });
       }
   }
