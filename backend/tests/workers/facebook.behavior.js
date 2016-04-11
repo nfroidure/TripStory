88281b171db437f6beddfefdbde914657b4de736
Nicolas Froidure <nicolas.froidure@insertafter.com>
2016-04-11 08:53:48 +0200

    First draft of facebook status retrieval

--- backend/tests/workers/facebook.behavior.js
+++ backend/tests/workers/facebook.behavior.js
@@ -4,6 +4,7 @@ var MongoClient = require('mongodb').MongoClient;
 var castToObjectId = require('mongodb').ObjectId;
 var sinon = require('sinon');
 var assert = require('assert');
+var Promise = require('bluebird');
 var nock = require('nock');
 var initObjectIdStub = require('objectid-stub');
 
@@ -29,7 +30,8 @@ describe('Facebook jobs', function() {
       .then(function(db) {
         context.db = db;
         done();
-      });
+      })
+      .catch(done);
   });
 
   afterEach(function(done) {
@@ -70,6 +72,17 @@ describe('Facebook jobs', function() {
     }], done);
   });
 
+  beforeEach(function(done) {
+    context.bus = {
+      trigger: sinon.spy(),
+    };
+    context.store = {
+      get: sinon.stub(),
+      set: sinon.stub(),
+    };
+    done();
+  });
+
   describe('for Facebook friends sync', function() {
     var friendsCall;
 
@@ -77,7 +90,7 @@ describe('Facebook jobs', function() {
       friendsCall = nock('https://graph.facebook.com:443', {
         encodedQueryParams: true,
       })
-      .get('/1664/friends')
+      .get('/v2.5/me/friends')
       .query({ access_token: 'COMMON_BOY' })
       .reply(200, {
         data: [{
@@ -88,7 +101,7 @@ describe('Facebook jobs', function() {
           id: '10153874940679789',
         }],
         paging: {
-          next: 'https://graph.facebook.com/v2.5/10153768131704201/friends',
+          next: 'https://graph.facebook.com/v2.5/1664/friends',
         },
         summary: {
           total_count: 189,
@@ -119,17 +132,292 @@ describe('Facebook jobs', function() {
         })
         .then(function() {
           friendsCall.done();
-          return context.db.collection('users').findOne({
-            _id: castToObjectId('abbacacaabbacacaabbacaca'),
-          }).then(function(user) {
+          return Promise.all([
+            context.db.collection('users').findOne({
+              _id: castToObjectId('abbacacaabbacacaabbacaca'),
+            }),
+            context.db.collection('users').findOne({
+              _id: castToObjectId('b17eb17eb17eb17eb17eb17e'),
+            }),
+          ]).spread(function(user, friend) {
             assert.deepEqual(user.friends_ids, [
               castToObjectId('b17eb17eb17eb17eb17eb17e'),
+            ]);
+            assert.deepEqual(friend.friends_ids, [
+              castToObjectId('abbacacaabbacacaabbacaca'),
             ]);
           });
         })
         .then(done.bind(null, null))
         .catch(done);
       });
+    });
+
+  });
+
+  describe('for Facebook statuses sync', function() {
+    var exchange = 'A_FB_SYNC';
+
+    afterEach(function(done) {
+      context.db.collection('events').deleteMany({}, done);
+    });
+
+    describe('when there are no running trips', function() {
+
+      it('should do nothing', function(done) {
+        facebookJobs[exchange](context, {
+          exchange: exchange,
+          contents: {},
+        })
+        .then(function() {
+          assert.deepEqual(context.bus.trigger.args, []);
+          return context.db.collection('events').count({})
+          .then(function(count) {
+            assert.equal(count, 0);
+          });
+        })
+        .then(done.bind(null, null))
+        .catch(done);
+
+      });
+
+    });
+
+    describe('when there are running trips', function() {
+      var facebookSatusesCall;
+      var newEventsIds;
+
+      beforeEach(function() {
+        context.store.get.returns(Promise.resolve());
+        context.store.set.returns(Promise.resolve());
+        newEventsIds = [
+          context.createObjectId.next(),
+          context.createObjectId.next(1),
+          context.createObjectId.next(2),
+        ];
+
+        facebookSatusesCall = nock('https://graph.facebook.com:443', {
+          encodedQueryParams: true,
+        })
+        .get('/v2.5/me/posts')
+        .query({
+          access_token: 'COMMON_BOY',
+          fields: 'description,caption,link,created_time,id,photos,' +
+          'application,from,icon,message,message_tags,name,picture,' +
+          'place,status_type,type',
+          since: Math.floor(context.time() / 1000),
+        })
+        .reply(200, {
+          data: [{
+            caption: 'tripstory.insertafter.com',
+            link: 'https://tripstory.insertafter.com/',
+            created_time: '2016-04-10T10:41:27+0000',
+            id: '1664_124039517996797',
+            from: {
+              name: 'Popol',
+              id: '1664',
+            },
+            icon: 'https://www.facebook.com/images/icons/post.gif',
+            message: 'Visit ',
+            name: 'tripstory.insertafter.com',
+            status_type: 'shared_story',
+            type: 'link',
+          }, {
+            link: 'https://www.facebook.com/photo.php?fbid=124030797997669&set=a.124030867997662.1073741826.100011722482255&type=3',
+            created_time: '2016-04-10T10:33:42+0000',
+            id: '1664_124030871330995',
+            from: {
+              name: 'Popol',
+              id: '1664',
+            },
+            icon: 'https://www.facebook.com/images/icons/photo.gif',
+            message: 'Pic!',
+            picture: 'https://scontent.xx.fbcdn.net/hphotos-xpt1/v/t1.0-0/p130x130/12932741_124030797997669_9209190374435741367_n.jpg?oh=c8fa3ef139ee6e405390f5fe40e992f8&oe=57744799',
+            status_type: 'added_photos',
+            type: 'photo',
+          }, {
+            created_time: '2016-04-10T10:33:02+0000',
+            id: '1664_124028311331251',
+            from: {
+              name: 'Popol',
+              id: '1664',
+            },
+            message: 'Hello world!',
+            status_type: 'mobile_status_update',
+            type: 'status',
+          }, {
+            caption: 'Born on August 8, 1980',
+            link: 'https://www.facebook.com/1664/posts/122704514796964',
+            created_time: '1980-08-08T07:00:00+0000',
+            id: '1664_122704514796964',
+            from: {
+              name: 'Popol',
+              id: '1664',
+            },
+            icon: 'https://www.facebook.com/images/icons/post.gif',
+            name: 'Born on August 8, 1980',
+            type: 'link',
+          }],
+          paging: {
+            previous: 'https://graph.facebook.com/v2.5/1664/posts?fields=description,caption,link,created_time,id,photos,application,from,icon,message,message_tags,name,picture,place,status_type,type&format=json&since=1460284887&access_token=CAAGxEMtZAMXoBAPkjiZBe8aLPXUIGZBEbEtdggcd2IEvmb7TL89XUrPQ3JLRkI8QLx0hIuuCYSpXTmnsbTXTOI8ZB0ZC3YsZBzfMIy8lEcftywFupYjoUjQLh8JBrBZAjE00JQ3NC9TmfmGzUFZBn5IFtM3n76wscsoVPK5Ee2FPYpuMQTYoT3snL3Wq7VxaTENNXtOLBj2X5QZDZD&limit=25&__paging_token=enc_AdDO14UdBfbcDlNZBVxE3XZCLCZCLPztcLpe3U4umjcZB0YiaRrQmzbJHngid9cJcO9ZCZBE2KjR3SfW5aXimwNfgVrZCwQph0itHJb8r89NIdqvyhnlwZDZD&__previous=1',
+            next: 'https://graph.facebook.com/v2.5/1664/posts?fields=description,caption,link,created_time,id,photos,application,from,icon,message,message_tags,name,picture,place,status_type,type&format=json&access_token=CAAGxEMtZAMXoBAPkjiZBe8aLPXUIGZBEbEtdggcd2IEvmb7TL89XUrPQ3JLRkI8QLx0hIuuCYSpXTmnsbTXTOI8ZB0ZC3YsZBzfMIy8lEcftywFupYjoUjQLh8JBrBZAjE00JQ3NC9TmfmGzUFZBn5IFtM3n76wscsoVPK5Ee2FPYpuMQTYoT3snL3Wq7VxaTENNXtOLBj2X5QZDZD&limit=25&until=334566000&__paging_token=enc_AdACBXz3EuK5vKxtOLZAOsE81mwVzeU6ymWX9qZBac9XtRRjRy0cwf9wpqEnj09h09cAEvz7WDG079tYSV4uVHIu4ZBoTohqFC2GrX3FyLSHNxYSgZDZD',
+          },
+        }, {
+          'access-control-allow-origin': '*',
+          'content-type': 'text/javascript; charset=UTF-8',
+          'x-fb-trace-id': 'FJgxiLd+P4g',
+          'x-fb-rev': '2168097',
+          etag: '"0483d18750c66dbbaae0810e280e05ef50358008"',
+          pragma: 'no-cache',
+          'cache-control': 'private, no-cache, no-store, must-revalidate',
+          'facebook-api-version': 'v2.5',
+          expires: 'Sat, 01 Jan 2000 00:00:00 GMT',
+          vary: 'Accept-Encoding',
+          date: 'Sat, 06 Feb 2016 10:01:53 GMT',
+          connection: 'close',
+        });
+      });
+
+      beforeEach(function(done) {
+        context.db.collection('events').insertOne({
+          _id: castToObjectId('babababababababababababa'),
+          contents: {
+            trip_id: castToObjectId('babababababababababababa'),
+            type: 'trip-start',
+          },
+          owner_id: castToObjectId('abbacacaabbacacaabbacaca'),
+          trip: {
+            friends_ids: [],
+            title: 'Lol',
+            description: 'Lol',
+            hash: 'lol',
+            car_id: castToObjectId('b17eb17eb17eb17eb17eb17e'),
+          },
+          created: {
+            seal_date: new Date(context.time()),
+            user_id: castToObjectId('abbacacaabbacacaabbacaca'),
+            ip: '::1',
+          },
+          modified: [{
+            seal_date: new Date(context.time()),
+            user_id: castToObjectId('abbacacaabbacacaabbacaca'),
+            ip: '::1',
+          }],
+        }, done);
+      });
+
+      it('should retrieve tweets', function(done) {
+        facebookJobs[exchange](context, {
+          exchange: exchange,
+          contents: {},
+        })
+        .then(function() {
+          facebookSatusesCall.done();
+          assert.deepEqual(context.bus.trigger.args, [[{
+            exchange: 'A_TRIP_UPDATED',
+            contents: {
+              trip_id: castToObjectId('babababababababababababa'),
+              event_id: newEventsIds[0],
+              users_ids: [castToObjectId('abbacacaabbacacaabbacaca')],
+            },
+          }], [{
+            exchange: 'A_TRIP_UPDATED',
+            contents: {
+              trip_id: castToObjectId('babababababababababababa'),
+              event_id: newEventsIds[1],
+              users_ids: [castToObjectId('abbacacaabbacacaabbacaca')],
+            },
+          }], [{
+            exchange: 'A_TRIP_UPDATED',
+            contents: {
+              trip_id: castToObjectId('babababababababababababa'),
+              event_id: newEventsIds[2],
+              users_ids: [castToObjectId('abbacacaabbacacaabbacaca')],
+            },
+          }]]);
+          return Promise.all([
+            context.db.collection('events').findOne({
+              _id: newEventsIds[0],
+            }),
+            context.db.collection('events').findOne({
+              _id: newEventsIds[1],
+            }),
+            context.db.collection('events').findOne({
+              _id: newEventsIds[2],
+            }),
+          ]).spread(function(event1, event2, event3) {
+            assert.deepEqual(event1, {
+              _id: newEventsIds[0],
+              contents: {
+                facebookId: '1664_124039517996797',
+                trip_id: castToObjectId('babababababababababababa'),
+                type: 'facebook-link',
+                geo: [],
+                text: 'Visit ',
+                user_name: 'Popol',
+              },
+              trip: {
+                friends_ids: [],
+                title: 'Lol',
+                description: 'Lol',
+                hash: 'lol',
+                car_id: castToObjectId('b17eb17eb17eb17eb17eb17e'),
+              },
+              owner_id: castToObjectId('abbacacaabbacacaabbacaca'),
+              created: {
+                seal_date: new Date('2016-04-10T10:41:27.000Z'),
+              },
+            });
+            assert.deepEqual(event2, {
+              _id: newEventsIds[1],
+              contents: {
+                facebookId: '1664_124030871330995',
+                trip_id: castToObjectId('babababababababababababa'),
+                type: 'facebook-photo',
+                geo: [],
+                text: 'Pic!',
+                user_name: 'Popol',
+              },
+              trip: {
+                friends_ids: [],
+                title: 'Lol',
+                description: 'Lol',
+                hash: 'lol',
+                car_id: castToObjectId('b17eb17eb17eb17eb17eb17e'),
+              },
+              owner_id: castToObjectId('abbacacaabbacacaabbacaca'),
+              created: {
+                seal_date: new Date('2016-04-10T10:33:42.000Z'),
+              },
+            });
+            assert.deepEqual(event3, {
+              _id: newEventsIds[2],
+              contents: {
+                facebookId: '1664_124028311331251',
+                trip_id: castToObjectId('babababababababababababa'),
+                type: 'facebook-status',
+                geo: [],
+                text: 'Hello world!',
+                user_name: 'Popol',
+              },
+              trip: {
+                friends_ids: [],
+                title: 'Lol',
+                description: 'Lol',
+                hash: 'lol',
+                car_id: castToObjectId('b17eb17eb17eb17eb17eb17e'),
+              },
+              owner_id: castToObjectId('abbacacaabbacacaabbacaca'),
+              created: {
+                seal_date: new Date('2016-04-10T10:33:02.000Z'),
+              },
+            });
+          });
+        })
+        .then(done.bind(null, null))
+        .catch(done);
+      });
+
     });
 
   });
--- backend/workers/facebook/facebook.jobs.js
+++ backend/workers/facebook/facebook.jobs.js
@@ -1,9 +1,12 @@
 'use strict';
 
+var workersUtils = require('../utils');
+var controllersUtils = require('../../app/utils/controllers');
 var request = require('request');
-
-var SERVER = 'https://graph.facebook.com/';
+var SINCE_ID_STORE_PREFIX = 'facebook:since:';
+var SERVER = 'https://graph.facebook.com/v2.5';
 var facebookJobs = {
+  A_FB_SYNC: facebookSyncJob,
   A_FB_SIGNUP: facebookSignupJob,
   A_FB_LOGIN: facebookLoginJob,
 };
@@ -24,10 +27,11 @@ function facebookLoginJob(context, event) {
   .then(pairFacebookFriends.bind(null, context));
 }
 
+// https://developers.facebook.com/docs/graph-api/reference/v2.5/user/friends
 function pairFacebookFriends(context, user) {
   return new Promise(function pairFriendsPromise(resolve, reject) {
     request.get(
-      SERVER + user.auth.facebook.id + '/friends?access_token=' +
+      SERVER + '/me/friends?access_token=' +
       user.auth.facebook.accessToken,
       function pairFriendsCallback(err, res, body) {
         if(err) {
@@ -74,5 +78,129 @@ function pairFacebookFriends(context, user) {
         .catch(reject);
       }
     );
+  });
+}
+
+// https://developers.facebook.com/docs/graph-api/reference/v2.5/user/feed
+function facebookSyncJob(context) {
+  return workersUtils.getCurrentTrips(context)
+  .then(function handleCurrentTrips(tripsEvents) {
+    if(!tripsEvents.length) {
+      return Promise.resolve();
+    }
+    return Promise.all(tripsEvents.map(function(tripEvent) {
+      context.logger.debug('Retrieving trip facebook users for:', tripEvent.trip.title);
+      return context.db.collection('users').find({
+        _id: { $in: tripEvent.trip.friends_ids.concat(tripEvent.owner_id) },
+        'auth.facebook': { $exists: true },
+      }).toArray()
+      .then(function(users) {
+        context.logger.debug('Found ' + users.length + ' facebook users for:', tripEvent.trip.title);
+        return Promise.all(users.map(function(user) {
+          var newSince;
+
+          context.logger.debug('Getting ' + user.contents.name + ' twitts');
+          return context.store.get(
+            SINCE_ID_STORE_PREFIX + tripEvent._id.toString() + ':' +
+            user._id.toString()
+          )
+          .then(function(since) {
+            since = since || Math.floor(
+              (tripEvent.created.seal_date).getTime() / 1000
+            );
+            return new Promise(function pairFriendsPromise(resolve, reject) {
+              request.get(
+                SERVER + '/me/posts' +
+                '?access_token=' + user.auth.facebook.accessToken +
+                '&fields=description,caption,link,created_time,id,photos,' +
+                'application,from,icon,message,message_tags,name,picture,place,' +
+                'status_type,type' +
+                '&since=' + since,
+                function retrieveStatusCallback(err, res, body) {
+                  var statuses;
+
+                  if(err) {
+                    return reject(err);
+                  }
+                  if(200 > res.statusCode || 300 <= res.statusCode) {
+                    return reject(new Error('E_BAD_RESPONSE'));
+                  }
+                  try {
+                    statuses = JSON.parse(body).data || [];
+                  } catch(err) {
+                    return reject(err);
+                  }
+                  context.logger.debug('Retrieved facebook statuses:', res.statusCode, body);
+                  context.logger.debug('Fetched ' + statuses.length +
+                    ' statuses sent by ' + user.contents.name, since);
+                  if(!statuses.length) {
+                    return resolve();
+                  }
+                  newSince = Math.round(
+                    (new Date(statuses[0].created_time)).getTime() / 1000
+                  );
+                  statuses = statuses.filter(function(status) {
+                    return status.message &&
+                      -1 !== ['photo', 'status', 'link'].indexOf(status.type);
+                  });
+                  Promise.all(statuses.map(function(status) {
+                    var statusDate = new Date(status.created_time);
+
+                    if(tripEvent.created.seal_date.getTime() > statusDate.getTime()) {
+                      return Promise.resolve();
+                    }
+                    return context.db.collection('events').findOneAndUpdate({
+                      'contents.facebookId': status.id,
+                    }, {
+                      $set: {
+                        'contents.facebookId': status.id,
+                        'contents.text': status.message,
+                        'contents.geo': status.place && status.place.location &&
+                          'undefined' === typeof status.place.location.latitude &&
+                          'undefined' === typeof status.place.location.longitude ? [
+                            status.place.location.latitude,
+                            status.place.location.longitude,
+                            0,
+                          ] : [],
+                        'contents.user_name': status.from.name,
+                      },
+                      $setOnInsert: {
+                        _id: context.createObjectId(),
+                        owner_id: user._id,
+                        'contents.trip_id': tripEvent._id,
+                        'contents.type': 'facebook-' + status.type,
+                        trip: tripEvent.trip,
+                        created: controllersUtils.getDateSeal(statusDate),
+                      },
+                    }, {
+                      upsert: true,
+                      returnOriginal: false,
+                    })
+                    .then(function(result) {
+                      context.bus.trigger({
+                        exchange: 'A_TRIP_UPDATED',
+                        contents: {
+                          trip_id: tripEvent._id,
+                          event_id: result.value._id,
+                          users_ids: tripEvent.trip.friends_ids.concat([tripEvent.owner_id]),
+                        },
+                      });
+                    });
+                  }))
+                  .then(context.store.set.bind(
+                    null,
+                    SINCE_ID_STORE_PREFIX + tripEvent._id.toString() + ':' +
+                    user._id.toString(),
+                    newSince
+                  ))
+                  .then(resolve)
+                  .catch(reject);
+                }
+              );
+            });
+          });
+        }));
+      });
+    }));
   });
 }
