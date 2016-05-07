17fd98bc98a775fe35de02b0fddbe04b86327134
Nicolas Froidure <nicolas.froidure@insertafter.com>
2016-05-07 13:32:35 +0200

    Makes the toaster native when possible

--- mobile/app/scripts/utils/toaster.service.js
+++ mobile/app/scripts/utils/toaster.service.js
@@ -4,24 +4,35 @@
   var TOAST_POSITION = 'top';
   var TOAST_DURATION = 2500;
 
+  // Since it looks like there is no way to guess the platform is
+  // a simple web nrowser, let's assume it is when a device has no props
+  var platformIsWeb = 0 == Object.keys(ionic.Platform.device()).length;
+
   angular
     .module('app.utils')
     .service('toasterService', ToasterService);
 
     ToasterService.$inject = [
+      '$cordovaToast',
       'ionicToast',
     ];
     /* @ngInject */
     function ToasterService(
+      ngCordova,
       ionicToast
     ) {
       return {
-        show: showToast,
+        show: platformIsWeb ?
+          showWebToast :
+          showNativeToast,
       };
 
       //
-      function showToast(message) {
+      function showWebToast(message) {
         ionicToast.show(message, TOAST_POSITION, false, TOAST_DURATION);
+      }
+      function showNativeToast(message) {
+        $cordovaToast.show(message, TOAST_DURATION, TOAST_POSITION);
       }
     }
 
--- mobile/app/scripts/utils/utils.module.js
+++ mobile/app/scripts/utils/utils.module.js
@@ -3,6 +3,7 @@
 
   angular
     .module('app.utils', [
+      'ngCordova',
       'ionic-toast',
     ]);
 
