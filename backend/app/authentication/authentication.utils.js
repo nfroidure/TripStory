1fce38794d96d8033a3ae33cfa8be47e66b07695
Nicolas Froidure <nicolas.froidure@insertafter.com>
2016-07-23 12:04:20 +0200

    Fixing the swagger api regexp

--- backend/app/authentication/authentication.utils.js
+++ backend/app/authentication/authentication.utils.js
@@ -73,7 +73,7 @@ function authenticationUtilsCreateDefaultRights() {
     methods: reaccess.READ_MASK,
   }, {
     // Swagger
-    path: '/(api|swagger)',
+    path: '/(api|swagger)$',
     methods: reaccess.READ_MASK,
   }, {
     // Ping
